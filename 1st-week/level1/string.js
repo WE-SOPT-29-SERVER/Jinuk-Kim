ce862fe43d8fb158fefe155ac0e975ecb2c6e3ca
NownS <sksmsx147@gmail.com>
2021-10-05 13:27:50 +0900

    1st week level1

---
+++ 1st-week/level1/string.js
@@ -0,0 +1,9 @@
+const str1 = "Just do it";
+const str2 = "Just don't do it";
+
+const myName = "김진욱";
+const strr = "안녕하세요" + myName + "입니다";
+const backtick = `안녕하세요 ${myName}입니다.`;
+console.log(myName);
+console.log(strr);
+console.log(backtick);
---
+++ 1st-week/level1/test.js
@@ -0,0 +1,18 @@
+// var : 변수 재선언/재할당 가능
+
+// var variableVar = "123";
+// variableVar = "321";
+
+// console.log("variableVar", variableVar);
+
+// let : 변수 재선언 불가능, 재할당 가능
+
+// let variableVar = "123";
+// variableVar = "321";
+// console.log("variableVar", variableVar);
+
+// const : 변수 재선언/재할당 불가능
+
+// const variableVar = "123";
+// variableVar = "321";
+// console.log("variableVar", variableVar);
