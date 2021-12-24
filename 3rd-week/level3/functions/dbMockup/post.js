38afc0df6f3f75053a5282901fb02243e4f7bd94
NownS <kimjuicy29@gmail.com>
2021-12-24 09:14:29 +0900

    3rd week level3

---
+++ 3rd-week/level3/functions/dbMockup/post.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        id: 1,
+        subject: "제목1",
+        content: "1번내용",
+    },
+    {
+        id: 2,
+        subject: "제목2",
+        content: "2번내용",
+    },
+    {
+        id: 3,
+        subject: "제목3",
+        content: "3번내용",
+    },
+];
